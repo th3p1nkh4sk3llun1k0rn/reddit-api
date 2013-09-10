2fcb823c79aca2b8a99777320e0f2603b9ea09a1
U-EUROPE\t-jusut <t-jusut@IFRIT.europe.corp.microsoft.com>
2013-09-10 21:42:32 +0100

    Added Reddithingy type and show instance

--- src/Reddit.hs
+++ src/Reddit.hs
@@ -2,3 +2,14 @@ module Reddit where
 
 import Network.Curl
 import Text.JSON
+
+data RedditThingy = Comment | Account | Link | Message | Subreddit | Award | PromoCampaign
+
+instance Show RedditThingy where
+  show Comment       = "t1"
+  show Account       = "t2"
+  show Link          = "t3"
+  show Message       = "t4"
+  show Subreddit     = "t5"
+  show Award         = "t6"
+  show PromoCampaign = "t8"
