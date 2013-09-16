122a2093e2707b43359a6f11a2b1b2e6db42ff46
U-EUROPE\t-jusut <t-jusut@IFRIT.europe.corp.microsoft.com>
2013-09-16 10:55:30 +0100

    temp login function, need to figure out Reddit Monad

--- src/Reddit.hs
+++ src/Reddit.hs
@@ -1,9 +1,11 @@
 module Reddit where
 
-import Network.Curl
+import Network.HTTP
 import Text.JSON
 
 data RedditThingy = Comment | Account | Link | Message | Subreddit | Award | PromoCampaign
+
+data User = User {hasMail :: Bool, name :: String, isFriend :: Bool, created :: Double, modHash :: String, createdUTC :: Double, linkKarma :: Int, commentKarma :: Int, over18 :: Bool, isGold :: Bool, isMod :: Bool, hasVerifiedEmail :: Bool, id :: String, hasModMail :: Bool}
 
 instance Show RedditThingy where
   show Comment       = "t1"
@@ -13,3 +15,18 @@ instance Show RedditThingy where
   show Subreddit     = "t5"
   show Award         = "t6"
   show PromoCampaign = "t8"
+
+login :: String -> String -> IO String
+login username password = simpleHTTP (postRequest $ "http://www.reddit.com/api/login?api_type=json&passwd=" ++ password ++ "&rem=false&user=" ++ username) >>= getResponseBody
+
+{-me :: IO User
+me = do
+  let s = "http://www.reddit.com/api/me.json"
+  case parseURI s of
+    Nothing -> undefined
+    Just uri -> do
+      tryGet <- httpGet uri
+      case tryGet of
+        Nothing -> undefined
+        Just content -> print content
+  undefined-}
